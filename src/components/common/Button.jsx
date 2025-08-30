870a24221f9d819d78cf0c49d9992cbb5aa1724c
Milton Ahmed <miltonahmed08@yahoo.com>
2025-08-30 21:57:55 +0400

    Update Button.jsx
    Dynamic button issue fixed

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,45 @@
-const Button = ({ text, bg = '#FF624C', fontName = 'montserrat',fontWeight='bold',size='xl', color ="white", paddingY="4", paddingX="10", radius="10" }) => {
+// const Button = ({ text, bg = '#FF624C', fontName = 'montserrat',fontWeight='bold',size='xl', color ="white", paddingY="4", paddingX="10", radius="10" }) => {
+//   return (
+//     <button
+//       className={`bg-[${bg}]  font-${fontName} font-${fontWeight}  text-${color}    rounded-[${radius}px] `}
+//       style={{
+//         fontSize:'20px',
+//         padding:paddingY+px+ " " + paddingX +"px",
+
+        
+//       }}
+//     >
+//       {text}
+//     </button>
+//   );
+// };
+
+// export default Button;
+
+
+const Button = ({
+  text,
+  bg = '#FF624C',
+  fontName = 'Montserrat',
+  fontWeight = 'bold',
+  size = '20px',
+  color = 'white',
+  paddingY = 12,
+  paddingX = 24,
+  radius = 10,
+}) => {
   return (
     <button
-      className={`bg-[${bg}]  font-${fontName} font-${fontWeight} text-${size} text-${color}  py-${paddingY} px-${paddingX}  rounded-[${radius}px] `}
+      style={{
+        backgroundColor: bg,
+        fontFamily: fontName,
+        fontWeight,
+        color,
+        fontSize: size,
+        padding: `${paddingY}px ${paddingX}px`,
+        borderRadius: `${radius}px`,
+      }}
+      className="shadow-md hover:opacity-90 transition cursor-pointer"
     >
       {text}
     </button>
@@ -9,3 +47,4 @@ const Button = ({ text, bg = '#FF624C', fontName = 'montserrat',fontWeight='bold
 };
 
 export default Button;
+
